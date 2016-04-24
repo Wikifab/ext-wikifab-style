6802f6253b8798520f5c7810abde69c7f44991d1
Pierre Boutet <boutetpierre@yahoo.fr>
2016-04-24 17:37:19 +0200

    modif menu utilisateur

--- wikifabstyle.php
+++ wikifabstyle.php
@@ -1,5 +1,7 @@
 <?php
 
+$wgMessagesDirs['WfExtStyle'] = __DIR__ . "/i18n";
+
 $wgResourceModules['ext.Wikifab.js'] = array(
 	'scripts' => 'wikifab-style.js',
 	'styles' => array(),
@@ -11,3 +13,19 @@ $wgResourceModules['ext.Wikifab.js'] = array(
 	'localBasePath' => __DIR__ . '',
 	'remoteExtPath' => 'WfExtensions/WikifabStyle',
 );
+
+// Specify the function that will change the user menu.
+$wgHooks['PersonalUrls'][] = 'PersonalUrlsWikifabCustom';
+function PersonalUrlsWikifabCustom( &$personal_urls, &$title, $this  ) {
+	unset($personal_urls['mytalk']);
+	if(isset($personal_urls['userpage']['text'])) {
+		$personal_urls['userpage']['text'] = wfMessage( 'wfextstyle-my_profil');
+	}
+	if(isset($personal_urls['anonuserpage'])) {
+		unset($personal_urls['anonuserpage']);
+	}
+	if(isset($personal_urls['anontalk'])) {
+		unset($personal_urls['anontalk']);
+	}
+	//var_dump($personal_urls);
+}
