79d4961e505f80953bb76a2933fadd930eff7e2c
Julien <julien@dokit.io>
2018-06-22 17:18:01 +0200

    back to top button

--- wikifab-style.js
+++ wikifab-style.js
@@ -37,16 +37,16 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
+        document.getElementById("backToTop").style.display = "block";
     } else {
-        document.getElementById("myBtn").style.display = "none";
+        document.getElementById("backToTop").style.display = "none";
     }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	$('body, html').stop().animate({scrollTop:0}, 500, 'swing', function() { 
+	});
 } 
 
 // BACK TO TOP - END
